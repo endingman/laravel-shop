ef8cd8d4dd2b315b39b591fa4ef4f0331c722779
endingman <631925002@qq.com>
2018-12-17 03:53:21 +0000

    众筹结束逻辑

---
+++ app/Console/Commands/Cron/FinishCrowdFunding.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands\Cron;
+
+use App\Models\CrowdfundingProduct;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class FinishCrowdFunding extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:finish-crowdfunding';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '结束众筹';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        CrowdfundingProduct::query()
+            ->with('product')
+        // 众筹结束时间早于当前时间
+            ->where('end_at', '<=', Carbon::now())
+        // 众筹状态为众筹中
+            ->where('status', CrowdfundingProduct::STATUS_FUNDING)
+            ->get()
+            ->each(function (CrowdfundingProduct $crowdfunding) {
+                // 如果众筹目标金额大于实际众筹金额
+                if ($crowdfunding->target_amount > $crowdfunding->total_amount) {
+                    // 调用众筹失败逻辑
+                    $this->crowdfundingFailed($crowdfunding);
+                } else {
+                    // 否则调用众筹成功逻辑
+                    $this->crowdfundingSucceed($crowdfunding);
+                }
+            });
+    }
+
+    public function crowdfundingSucceed()
+    {
+        // 只需将众筹状态改为众筹成功即可
+        $crowdfunding->update([
+            'status' => CrowdfundingProduct::STATUS_SUCCESS,
+        ]);
+    }
+
+    public function crowdfundingFailed()
+    {
+        // 将众筹状态改为众筹失败
+        $crowdfunding->update([
+            'status' => CrowdfundingProduct::STATUS_FAIL,
+        ]);
+
+        dispatch(new RefundCrowdfundingOrders($crowdfunding));
+    }
+}
