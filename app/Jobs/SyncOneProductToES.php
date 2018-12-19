1a03c258f29e11638a2165bfec211ac313f9d3bc
endingman <631925002@qq.com>
2018-12-19 07:35:26 +0000

    同步商品数据至 Elasticsearch

---
+++ app/Jobs/SyncOneProductToES.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SyncOneProductToES implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    protected $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $data = $this->product->toESArray();
+
+        app('es')->index([
+            'index' => 'products',
+            'type'  => '_doc',
+            'id'    => $data['id'],
+            'body'  => $data,
+        ]);
+    }
+}
